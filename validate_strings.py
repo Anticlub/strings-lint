cae97ef7ecdb017666b2d317083178fc9ea339cf
Anticlub <cristofdzfdz@gmail.com>
2026-02-24 11:36:39 +0100

    feat: validate key/vale structure and quoting in .strings entries

--- validate_strings.py
+++ validate_strings.py
@@ -5,6 +5,7 @@ import sys
 
 def main():
     errors = []
+    files_with_errors = set()
     
     parser = argparse.ArgumentParser(description="Validate .strings files")
     parser.add_argument("--root", default=".", help="Root directory to scan")
@@ -42,16 +43,54 @@ def main():
                             "code": "MISSING SEMICOLON",
                             "snipet": line[:80]
                         }
-                        
                         errors.append(error)
-                
+                        continue
+                    else:
+                        if "=" not in line:
+                            error = {
+                                "file": str(file),
+                                "line": line_number,
+                                "code": "MISSING =",
+                                "snipet": line[:80]
+                            }
+                            errors.append(error)
+                            continue
+                        else:
+                            parts = line[:-1].split("=", 1) 
+                            left = parts[0].strip()
+                            rigth = parts[1].strip()
+
+                            if (not left.startswith('"')) or (not left.endswith('"')):
+                                error = {
+                                    "file": str(file),
+                                    "line": line_number,
+                                    "code": "INVALID_KEY_QUOTING",
+                                    "snipet": line[:80]
+                                }
+                                errors.append(error)
+                                continue
+                            if (not rigth.startswith('"')) or (not rigth.endswith('"')):
+                                error = {
+                                    "file": str(file),
+                                    "line": line_number,
+                                    "code": "INVALID_VALUE_QUOTING",
+                                    "snipet": line[:80]
+                                }
+                                errors.append(error)
+                                continue
+
         except UnicodeDecodeError:
             print(f"Encoding error in file: {file}")
-            
-
-            
+    
+    for error in errors:
+        files_with_errors.add(error["file"])
+    
+    print("---")
+    print(f"Files scanned: {len(strings_files)}")
+    print(f"Files with errors: {len(files_with_errors)}")
     print(f"Total errors: {len(errors)}")
     print(errors)
+    
     if len(errors) > 0:
         sys.exit(1)
     else:
