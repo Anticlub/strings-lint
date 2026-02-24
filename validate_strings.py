a6ab75d6794f4a657e864f1891558e50d11b71fd
Anticlub <cristofdzfdz@gmail.com>
2026-02-24 10:49:22 +0100

    feat: collect lint errors in strutured format

--- validate_strings.py
+++ validate_strings.py
@@ -1,8 +1,11 @@
 import argparse
 from pathlib import Path
+import sys
 
 
 def main():
+    errors = []
+    
     parser = argparse.ArgumentParser(description="Validate .strings files")
     parser.add_argument("--root", default=".", help="Root directory to scan")
     args = parser.parse_args()
@@ -18,12 +21,41 @@ def main():
     
     print("Scanning root:", root_path)
     
-    string_files = list(root_path.rglob("*.strings"))
-    print(f"Found {len(string_files)} .strings files")
+    strings_files = list(root_path.rglob("*.strings"))
+    print(f"Found {len(strings_files)} .strings files")
     
-    for file in string_files:
-        print("-", file)
-    
+    for file in strings_files:
+        try:
+            with open(file, "r", encoding="utf-8") as f:
+                for line_number, line in enumerate(f, start=1):
+                    line = line.strip()
+                    if line == "":
+                        continue
+                    if line.startswith("//"):
+                        continue
+                    if line.startswith("/*"):
+                        continue
+                    if not line.endswith(";"):
+                        error = {
+                            "file": str(file),
+                            "line": line_number,
+                            "code": "MISSING SEMICOLON",
+                            "snipet": line[:80]
+                        }
+                        
+                        errors.append(error)
+                
+        except UnicodeDecodeError:
+            print(f"Encoding error in file: {file}")
+            
+
+            
+    print(f"Total errors: {len(errors)}")
+    print(errors)
+    if len(errors) > 0:
+        sys.exit(1)
+    else:
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
